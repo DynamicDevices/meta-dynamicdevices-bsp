3e2d741a78298b7ab6f6c8ec9c8f987cde84e1b1
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2026-09-02 12:15:10 +0100

    fix(screen): carry shared i.MX8MM pad definitions

---
+++ recipes-bsp/device-tree/lmp-device-tree/imx8mm-sw_pad_ctl-fields.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+#ifndef __IMX8MM_SW_PAD_CTL_FIELDS_H
+#define __IMX8MM_SW_PAD_CTL_FIELDS_H
+
+#define IMX8MM_SW_PAD_CTL_MASK_TRAILING         0x1ff
+#define IMX8MM_SW_PAD_CTL_DSE_X1                 0x0
+#define IMX8MM_SW_PAD_CTL_DSE_X2                 0x4
+#define IMX8MM_SW_PAD_CTL_DSE_X4                 0x2
+#define IMX8MM_SW_PAD_CTL_DSE_X6                 0x6
+#define IMX8MM_SW_PAD_CTL_FSEL_SLOW              0x0
+#define IMX8MM_SW_PAD_CTL_FSEL_FAST              0x10
+#define IMX8MM_SW_PAD_CTL_ODE_DIS                0x0
+#define IMX8MM_SW_PAD_CTL_ODE_EN                 0x20
+#define IMX8MM_SW_PAD_CTL_PUE_DOWN               0x0
+#define IMX8MM_SW_PAD_CTL_PUE_UP                 0x40
+#define IMX8MM_SW_PAD_CTL_HYS_CMOS               0x0
+#define IMX8MM_SW_PAD_CTL_HYS_SCHMITT            0x80
+#define IMX8MM_SW_PAD_CTL_PE_DIS                 0x0
+#define IMX8MM_SW_PAD_CTL_PE_EN                  0x100
+
+#endif /* __IMX8MM_SW_PAD_CTL_FIELDS_H */
